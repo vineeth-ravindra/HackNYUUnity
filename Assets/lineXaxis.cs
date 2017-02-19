1d3d740c12ff2620073e9ff80d1148ceea0c41c8
vineeth-nyllabs <vineeth@iOS-Vineeth.local>
2017-02-19 12:49:03 -0500

    Adding files

---
+++ Assets/lineXaxis.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineXaxis : MonoBehaviour {
+	private LineRenderer lineRenderer;
+	public Transform sourcex;
+	public Transform destinationx;
+
+	// Use this for initialization
+	void Start () {
+		sourcex.position = new Vector3 (0.0f, 0.0f, 0.0f);
+		destinationx.position = new Vector3(10.0f, 0.0f, 0.0f);
+		lineRenderer = GetComponent<LineRenderer> ();
+		lineRenderer.SetWidth(.3f,.3f);
+		lineRenderer.SetPosition(0,sourcex.position);
+		lineRenderer.SetPosition(1, destinationx.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
